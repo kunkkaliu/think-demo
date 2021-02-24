ad9749f477024ecc359af78ad89a7f6be138e9c9
kunkkaliu <zudapang@qq.com>
2021-02-24 12:46:32 +0800

    fix: husky使用问题

--- view/app.jsx
+++ view/app.jsx
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/home';
 import List from './pages/list';
 
-function App(props) {
+function App() {
   return (
     <Switch>
       <Redirect exact from="/" to="/home" />
